bf2a54564de7c055c87c68a479d7c8d5dfb7b832
Chris Franklin <cf73021@gmail.com>
2018-07-25 16:16:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a, b|
-     self[a], self[b] = self[b], self[a]
+  array[1], array[2] = array[2], array[1]
   end
 end
